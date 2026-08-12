589ecab2e1c167a4022e33c7c1a5f30636a5a816
Kavin-nilaa <162492723+Kavin-nilaa@users.noreply.github.com>
2026-08-13 04:28:38 +0530

    Implement Basepage class for Selenium WebDriver
    Added a Basepage class for Selenium WebDriver with methods for URL navigation, element interaction, and browser control.

--- Pages/base_page.py
+++ Pages/base_page.py
@@ -1,1 +1,31 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+class Basepage:
+
+    def __init__(self,driver):
+        self.driver=driver
+        self.wait=WebDriverWait(driver,10)
+
+    def open_url(self,url):
+        self.driver.get(url)
+
+    def element_displayed_enabled(self,locator):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element.is_enabled()
+        return element.is_displayed() and element.is_enabled()
+
+    def enter_text(self,locators,value):
+        element = self.wait.until(EC.visibility_of_element_located(locators))
+        element.clear()
+        element.send_keys(value)
+
+    def click(self,locator):
+        click_element = self.wait.until(EC.element_to_be_clickable(locator))
+        click_element.click()
+
+    def get_text(self,locator):
+        return self.wait.until(EC.visibility_of_element_located(locator)).text
+
+    def close_browser(self):
+        self.driver.quit()
